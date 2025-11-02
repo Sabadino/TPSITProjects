e75f153e37bcf60bfc86d7b83f5195f7419a8b89
Sabadino <hadikhammoud@icloud.com>
2025-11-02 16:21:15 +0100

    Quarto Commit (Aggiunta UI e variabili)

--- chrono/lib/main.dart
+++ chrono/lib/main.dart
@@ -26,15 +26,44 @@ class ChronoScreen extends StatefulWidget {
 }
 
 class _ChronoScreenState extends State<ChronoScreen> {
+  int totalSeconds = 0;
+  bool running = false;
+  bool paused = false;
+
+  String getTimeText() {
+    int min = totalSeconds ~/ 60;
+    int sec = totalSeconds % 60;
+    return '${min.toString().padLeft(2, '0')}:${sec.toString().padLeft(2, '0')}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text('Chrono'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text('00:00', style: TextStyle(fontSize: 60)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Text(
+                getTimeText(),
+                style: const TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
