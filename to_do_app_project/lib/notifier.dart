a2db762a39017a9e800704137a848f15e0038fa8
busixx-hub <riccardo.busetto@itiszuccante.edu.it>
2026-01-12 13:48:09 +0100

    Commit Forza Inter

--- to_do_app_project/lib/notifier.dart
+++ to_do_app_project/lib/notifier.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/widgets.dart';
-
 import 'model.dart';
 
 class TodoListNotifier with ChangeNotifier {
   final _todos = <Todo>[];
-
+  
   int get length => _todos.length;
-
+  
   void addTodo(String name) {
     _todos.add(Todo(name: name, checked: false));
     notifyListeners();
   }
-
+  
   void changeTodo(Todo todo) {
     todo.checked = !todo.checked;
     notifyListeners();
   }
-
+  
   void deleteTodo(Todo todo) {
     _todos.remove(todo);
     notifyListeners();
   }
-
+  
+  void updateNote(Todo todo, String note) {
+    todo.note = note;
+    notifyListeners();
+  }
+  
   Todo getTodo(int i) => _todos[i];
 }
-
--- to_do_app_project/lib/widgets.dart
+++ to_do_app_project/lib/widgets.dart
@@ -5,34 +5,48 @@ import 'model.dart';
 import 'notifier.dart';
 
 class TodoItem extends StatelessWidget {
-  TodoItem({required this.todo}) : super(key: ObjectKey(todo));
+  const TodoItem({
+    super.key,
+    required this.todo,
+  });
 
   final Todo todo;
 
-  TextStyle? _getTextStyle(bool checked) {
-    if (!checked) return null;
-
-    return const TextStyle(
-      color: Colors.black45,
-      decoration: TextDecoration.lineThrough,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    final TodoListNotifier notifier = context.watch<TodoListNotifier>();
+    final notifier = context.watch<TodoListNotifier>();
 
-    return ListTile(
-      onTap: () {
-        notifier.changeTodo(todo);
-      },
-      onLongPress: (() {
+    return GestureDetector(
+      onLongPress: () {
         notifier.deleteTodo(todo);
-      }),
-      leading: CircleAvatar(child: Text(todo.name[0])),
-      title: Text(todo.name, style: _getTextStyle(todo.checked)),
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            children: [
+              Checkbox(
+                value: todo.checked,
+                onChanged: (_) {
+                  notifier.changeTodo(todo);
+                },
+              ),
+              const SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  todo.name,
+                  style: TextStyle(
+                    fontSize: 16,
+                    decoration:
+                        todo.checked ? TextDecoration.lineThrough : null,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-
-    
